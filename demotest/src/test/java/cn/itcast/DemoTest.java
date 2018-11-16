b0f2fe570593d8a35d0c2e3ae40a6321209c4d0c
yujinxing <1481617980@qq.com>
2018-11-16 09:43:18 +0800

    代码修改

--- demotest/src/test/java/cn/itcast/DemoTest.java
+++ demotest/src/test/java/cn/itcast/DemoTest.java
@@ -3,5 +3,6 @@ package cn.itcast;
 public class DemoTest {
     public static void main(String[] args) {
         System.out.println("1241242412");
+        System.out.println("22222222222");
     }
 }
